bf06b0c1d9632f243f9fdcb73f94e55b4037b5fd
nonamebos <zuzoro47@gmail.com>
2025-05-17 11:09:46 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "zuzoro47.workers.dev.me"; // Ganti dengan domain utama kalian
-const serviceName = "zuzoro47"; // Ganti dengan nama workers kalian
+const rootDomain = "zuzoro47.workers.dev"; // Ganti dengan domain utama kalian
+const serviceName = "zuzoro47."; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
